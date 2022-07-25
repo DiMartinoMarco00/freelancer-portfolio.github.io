e7193bcde668551695d1b36752f92016e9989a10
unknown <dimartinomarco00@gmail.com>
2022-07-25 14:00:43 +0200

    first commit

---
+++ JS/menu-toggle.js
@@ -0,0 +1,11 @@
+var sideMenu = document.getElementById('side-menu');
+
+function openMenu()
+{
+    sideMenu.style.right = "0";
+}
+
+function closeMenu()
+{
+    sideMenu.style.right = "-175px";
+}
